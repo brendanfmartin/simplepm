bd6bd17d9ca03b4ff0fff01eb6f8f21cce3c4964
Brendan Martin <brendan.martin@jacquette.com>
2014-07-09 09:07:39 -0400

    bringing jquery in locally, updating data.js for ... something last night

--- public/js/data.js
+++ public/js/data.js
@@ -11,6 +11,9 @@ Storage.prototype.getObject = function(key) {
     return value && JSON.parse(value);
 }
 
+
+
+
 // get method for retrieving all JSON objects in localStorage
 Storage.prototype.getAllObjects = function() {
 	var arr = [];
@@ -35,5 +38,23 @@ Storage.prototype.getAllObjectNames = function() {
 // get method for retrieving all JSON objects in localStorage
 Storage.prototype.getSingleObject = function() {
 	var key = localStorage.key(0), value = localStorage[key];
-	return JSON.parse(JSON.parse(value));
+	return value && JSON.parse(value);
+}
+
+
+//
+Storage.prototype.updateProject = function(project, projectObject) {
+
+	localStorage.setItem( project, JSON.stringify(projectObject));
+    
+};
+
+
+// add task to project
+Storage.prototype.addTask = function(project, task) {
+	var projectObject = localStorage.getObject(project);
+	var taskNum = Object.keys(projectObject.tasks).length;
+	projectObject.tasks[taskNum] = task;
+	console.log(typeof projectObject.title, typeof projectObject);
+	localStorage.updateProject(projectObject.title, projectObject);
 }
--- public/js/main.js
+++ public/js/main.js
@@ -4,10 +4,25 @@
 
 $(function(){
 
+
+
 	var printProject = function(project){
 		$('input.project_name_input').val(project.title);
 		$('input.project_description_input').val(project.description);
+		var tasksLength = Object.keys(project.tasks).length;
+		for (var i = 0; i < tasksLength; i++) {
+			$('.task_list').append('<div class="task">'+project.tasks[i]+'</div>')
+		};
 	}
+
+
+	// var textarea = $('textarea_expand')
+	// var limit = 200;
+
+	// textarea.oninput = function() {
+	//   textarea.style.height = "";
+	//   textarea.style.height = Math.min(textarea.scrollHeight, 300) + "px";
+	// };
 
 	var firstObject = localStorage.getSingleObject();
 	if(firstObject) {
@@ -17,13 +32,12 @@ $(function(){
 	}
 
 	// change to function named enterToTab
-	$('body').on('keydown', 'input', function(e){
+	
+	$('.project_name_input, .project_description_input').on('keydown', function(e){
 		var self = $(this);
 		if(e.keyCode == 13) {
 			var focusable = $('body').find('input').filter(':visible');
-
 			var next = focusable.eq(focusable.index(this)+1);
-			
 			if(next.length) {
 				next.focus();
 			}
@@ -41,7 +55,7 @@ $(function(){
 			try {
 				// check to see if project already exists
 				var newProject = new Project(projectTitle, projectDescription);
-				localStorage.setObject(projectTitle, JSON.stringify(newProject));
+				localStorage.setObject(projectTitle, (newProject));
 				var confirmNotification = new Notification('Project Saved!', 'info');
 				confirmNotification.alert();
 			} catch(e) {
@@ -53,51 +67,33 @@ $(function(){
 	});
 
 
-	
-	
-	// $('.project_description').html('<p>'+Project.description+'</p>');
-
-
-	
-	
-
-
-	var $task_list = $('.task_list');
-	$('.task_input').on('blur', function(e){
-		if($('.task_input')[0].value != ''){
-			var data = $('.task_input');
-			console.log(data[0].value);
-			$task_list.append('<div class="task">' +
-			'<span value="'+data[0].value+'" id="'+data[0].value+'" />'
-			+ data[0].value
-			+'</div>');
+	$('.project_name_input, .project_description_input').on('keydown', function(e){
+		var self = $(this);
+		if(e.keyCode == 13) {
+			var focusable = $('body').find('input').filter(':visible');
+			var next = focusable.eq(focusable.index(this)+1);
+			if(next.length) {
+				next.focus();
+			}
+			return false;
 		}
 	});
 
-
-	// $task_list.append('<div class="task">' +
-	// 		'<span value="'+data.task_value+'" class="'+data.task_class+' task_title" id="'+data.task_name+'" />'
-	// 		+ data.task_name
-	// 		+'<span class="task_delete">x</span>'
-	// 		+'</div>');
+	
+	$('.task_input').on('keyup', function(e){
+		var self = $(this);
+		if(e.keyCode == 13) {
+			if($('.task_input')[0].value != ''){
+				var task = $('.task_input')[0].value;
+				var project = $('.project_name_input')[0].value;
+				localStorage.addTask(project,task);
+				$('.task_list').append('<div class="task">'+task+'</div>');
+				$('.task_input').val('');
+			}
+		}
 		
-	
-		
+	});
 
-	// $('#new_task').submit(function(e){
-	// 	e.preventDefault();
-	// 	var values = {};
-	// 	$.each($('#new_task').serializeArray(), function(i, field) {
-	// 	    values[field.name] = field.value;
-	// 	});
-	// 	if(values['task_name'].length > 0) {
-	// 		socket.emit('new_task', values);
-	// 	}
-	// 	$('#new_task').trigger('reset');
-	// })
-
-	
-	
 
 
 	
